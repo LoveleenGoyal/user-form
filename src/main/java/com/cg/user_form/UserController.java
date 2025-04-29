cddbc389173e14be5ee70521f6e121af6a3b9ce7
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-29 11:25:16 +0530

    [Loveleen] stats: Created UserModel and Controller

--- src/main/java/com/cg/user_form/UserController.java
+++ src/main/java/com/cg/user_form/UserController.java
@@ -1,2 +1,49 @@
-package com.cg.user_form;public class UserController {
+package com.cg.user_form;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+@Controller
+@SessionAttributes("user")
+public class UserController {
+
+    @ModelAttribute("user")
+    public UserModel setUpUserForm() {
+        return new UserModel();
+    }
+
+    @GetMapping("/")
+    public String showFirstPage() {
+        return "first-page";
+    }
+
+    @PostMapping("/page2")
+    public String goToSecondPage(@ModelAttribute("user") UserModel user,
+                                 @RequestParam String firstName,
+                                 @RequestParam String lastName) {
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return "second-page";
+    }
+
+    @PostMapping("/page3")
+    public String goToThirdPage(@ModelAttribute("user") UserModel user,
+                                @RequestParam String email,
+                                @RequestParam String phone) {
+        user.setEmail(email);
+        user.setPhone(phone);
+        return "third-page";
+    }
+
+    @PostMapping("/page4")
+    public String goToFourthPage(@ModelAttribute("user") UserModel user,
+                                 @RequestParam String city,
+                                 @RequestParam String country,
+                                 SessionStatus status) {
+        user.setCity(city);
+        user.setCountry(country);
+        return "summary";
+    }
 }
--- src/main/java/com/cg/user_form/UserModel.java
+++ src/main/java/com/cg/user_form/UserModel.java
@@ -1,8 +1,6 @@
 package com.cg.user_form;
 
-import lombok.Data;
 
-@Data
 public class UserModel {
     private String firstName;
     private String lastName;
@@ -10,4 +8,53 @@ public class UserModel {
     private String phone;
     private String city;
     private String country;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
+
