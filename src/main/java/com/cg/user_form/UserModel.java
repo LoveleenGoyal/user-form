6edd662a4bc84643f0ffd4bfcd0931d7c929584e
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-29 10:14:51 +0530

    [Loveleen] stats: Created a user model

---
+++ src/main/java/com/cg/user_form/UserModel.java
@@ -0,0 +1,13 @@
+package com.cg.user_form;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String city;
+    private String country;
+}
